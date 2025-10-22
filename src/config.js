f1771184ca56266ddcd0befd6d0a32a1701f8757
sussulussate-lab <sussulussate@gmail.com>
2025-10-22 10:06:07 +0100

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,8 @@
+// ===============================
+// ARQUIVO: config.js
+// Aqui colocamos o endereço do backend
+// ===============================
+
+// Altere essa URL para o seu backend assim que estiver hospedado!
+const API_BASE_URL = "http://localhost:3000";
+console.log("🌐 Backend configurado em:", API_BASE_URL);
